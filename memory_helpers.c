af145e2f4aa59cee08457617a6b8f40b936eca47
NAttyXO <nattyxo2018@gmail.com>
2022-08-09 11:57:14 -0700

    update memory_helpers

--- memory_helpers.c
+++ memory_helpers.c
@@ -9,19 +9,12 @@ void free_buffer(buffer_t *output);
 buffer_t *init_buffer(void);
 
 
-
 /**
-
  * _memcpy - Copies n bytes from memory area src to
-
  *           the buffer contained in a buffer_t struct.
-
  * @output: A buffer_t struct.
-
  * @src: A pointer to the memory area to copy.
-
  * @n: The number of bytes to be copied.
-
  *
 
  * Return: The number of bytes copied.
